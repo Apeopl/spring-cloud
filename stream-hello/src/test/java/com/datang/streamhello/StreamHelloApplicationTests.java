42544da3c2b9dee1937e87898a653418511bb65e
zhengjinlei <zhengjinlei@taijihuabao.com>
2020-03-10 17:54:54 +0800

    消息驱动

--- stream-hello/src/test/java/com/datang/streamhello/StreamHelloApplicationTests.java
+++ stream-hello/src/test/java/com/datang/streamhello/StreamHelloApplicationTests.java
@@ -1,8 +1,24 @@
 package com.datang.streamhello;
 
-class StreamHelloApplicationTests {
+import com.datang.streamhello.biz.SinkSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-	void contextLoads() {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = StreamHelloApplication.class)
+@WebAppConfiguration
+public class StreamHelloApplicationTests {
+	@Autowired
+	private SinkSender sinkSender;
+
+	@Test
+	public void contextLoads() {
+		sinkSender.output().send(MessageBuilder.withPayload("from sinksender").build());
 	}
 
 }
